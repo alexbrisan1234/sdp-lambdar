b7df66da9915e8b4083dc48c394bb5d7acb3a1da
alexbrisan <s1505883@sms.ed.ac.uk>
2018-02-06 11:28:55 +0000

    Changed motor implementation and added tests

--- src/code/control/motor.py
+++ src/code/control/motor.py
@@ -7,14 +7,36 @@ class Motor:
         
     def run(self, duration, speed):
         self.motor.run_timed(time_sp = duration, speed_sp = speed)
-
-    def set_polarity(self, polarity):
-        if polarity == -1:
-            self.motor.polarity = ev3.LargeMotor.POLARITY_INVERSED
-        elif polarity == 1:
-            self.motor.polarity = ev3.LargeMotor.POLARITY_NORMAL
-        else:
-            raise ValueError("Unknown value of polarity entered. Please use -1 or 1")
             
     def stop(self):
         self.motor.stop()
+
+class ArnoldMotors:
+    def __init__(self, portDict):
+        self . frontLeft = Motor(portDict['frontLeft'])
+        self . frontRight = Motor(portDict['frontRight'])
+        self . rearLeft = Motor(portDict['rearLeft'])
+        self . rearRight = Motor(portDict['rearRight'])
+
+        self . left = [self.frontLeft, self.rearLeft]
+        self . right = [self.frontRight, self.rearRight]
+
+        self . motors = self . left + self . right
+        
+    def turn(self, duration, speedLeft, speedRight):
+        # Run the left side
+        for leftMotor in self . left:
+            leftMotor . run(duration, speedLeft)
+
+        # Run the right side
+        for rightMotor in self . right:
+            rightMotor . run(duration, speedRight)
+
+    def move_forwards(self, duration, speed):
+        for arnoldMotor in self.motors:
+            arnoldMotor . run(duration, speed)
+
+    def move_backwards(self, duration, speed):
+        for arnoldMotor in self.motors:
+            # Multiply speed by -1 to achieve backwards motion
+            arnoldMotor.run(duration, -1 * speed)
