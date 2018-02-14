ad2379f4a160a86288035ebc437b7199ac6ddb70
MichaelHax0rL33t <michaelides.michael.1193.g2@gmail.com>
2018-02-14 17:29:39 +0000

    Added speed measurement method on Motor and isMoving and stop on ArnoldMotors

--- src/code/control/motor.py
+++ src/code/control/motor.py
@@ -1,4 +1,6 @@
 import ev3dev.ev3 as ev3
+from functools import reduce
+from math import pi
 
 class Motor:
 
@@ -10,7 +12,29 @@ class Motor:
             
     def stop(self):
         self.motor.stop()
-
+        
+    ''' 
+    measures the speed according to revs / sec
+    NOTICE: even if there's something blocking the robot
+    the wheel keeps turning and hence the measurement is inaccurate
+    ''' 
+    def getSpeed(self):
+        wheelDiameter = 8.2 #cm
+        wheelCircumference = 2*pi*wheelDiameter/2 #cm
+        speedSum = 0
+        count = 0
+        while (self.motor.is_running and count < 10):
+            # revs / sec = (degrees / sec) / ((degrees / rotation)=360)
+            bigGearRps = (self.motor.speed / self.motor.count_per_rot)
+            # big gear has 40 teeth, small has 24
+            rps = bigGearRps * 40 / 24
+            #instantaneous speed
+            speedSum = speedSum + wheelCircumference * rps
+            count = count + 1
+        if (count == 0):
+            return 0
+        return round(speedSum/count)
+        
 class ArnoldMotors:
     def __init__(self, portDict):
         self . frontLeft = Motor(portDict['frontLeft'])
@@ -40,3 +64,10 @@ class ArnoldMotors:
         for arnoldMotor in self.motors:
             # Multiply speed by -1 to achieve backwards motion
             arnoldMotor.run(duration, -1 * speed)
+            
+    def isMoving(self):
+        #return reduce((lambda m1, m2: m1.motor.is_running or m2.motor.is_running), self.motors)
+        return any([m.motor.is_running for m in self.motors])
+    def stop(self):
+       for motor in self.motors:
+           motor.stop()
