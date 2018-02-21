ffd711fc95e1de5f500393aa3b3d4a262fd08817
Braňo Pilňan <B.Pilnan@gmail.com>
2018-02-21 21:30:29 +0000

    Refactor direction sensing for tracking
    As Theo suggested, we are not waiting for a reply on radio anymore since
    it adds unnecessary delay. We only send request and wait for the
    ultrasonic pulse.
    I also switched from using digitalRead() to port manipulation.

--- arduino/tracker/sender.ino
+++ arduino/tracker/sender.ino
@@ -1,28 +1,28 @@
-const byte pinTrig = 7;
+const byte pinTrig = 6;
 
-void setup(){
-Serial.begin(115200);
-pinMode(8,OUTPUT); // switch on the radio
-digitalWrite(8,HIGH);
-pinMode(4,OUTPUT); // switch on the radio
-digitalWrite(4,LOW); // ensure the radio is not sleeping
-pinMode(pinTrig, OUTPUT);
+void activateUltrasonic(int trigPin)
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 }
 
-void listen(){
- digitalWrite(pinTrig, LOW);
- delayMicroseconds(2);
- digitalWrite(pinTrig, HIGH);
- delayMicroseconds(10);
- digitalWrite(pinTrig, LOW);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(8,OUTPUT); // switch on the radio
+  digitalWrite(8,HIGH);
+  pinMode(4,OUTPUT); // switch on the radio
+  digitalWrite(4,LOW); // ensure the radio is not sleeping
+  pinMode(pinTrig, OUTPUT);
+  digitalWrite(pinTrig, LOW);
+  delay(1000);
 }
 
-void loop(){
- 
- while(!Serial.find("$"));
- Serial.println("&");
- Serial.flush();
- delay(23);
- listen();
- 
+void loop()
+{
+  while(!Serial.find("$$$"));
+  activateUltrasonic(pinTrig);
 }
