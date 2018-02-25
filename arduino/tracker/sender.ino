04a51a8acf91dad13b5df2ae375a9141be41a143
stmfunk <renegadethunder@gmail.com>
2018-02-25 14:18:28 +0000

    Reverted changes made to tracker folder to previous commit. Restored sender.ino file

---
+++ arduino/tracker/sender.ino
@@ -0,0 +1,28 @@
+const byte pinTrig = 6;
+
+void activateUltrasonic(int trigPin)
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(8,OUTPUT); // switch on the radio
+  digitalWrite(8,HIGH);
+  pinMode(4,OUTPUT); // switch on the radio
+  digitalWrite(4,LOW); // ensure the radio is not sleeping
+  pinMode(pinTrig, OUTPUT);
+  digitalWrite(pinTrig, LOW);
+  delay(1000);
+}
+
+void loop()
+{
+  while(!Serial.find("$$$"));
+  activateUltrasonic(pinTrig);
+}
