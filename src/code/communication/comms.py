f42e39a95735e97c409a79dd26ee8a9c9db8171d
Donal O'Shea <renegadethunder@gmail.com>
2018-03-01 04:19:44 +0000

    Changes to the signal processing code (#20)
    * Added comments, error catch for incorrect usb port, and changed constants to class variables
    
    * Changed ir durations to variables
    
    * Fixed serial backlog issue. Refactored Message class and changed interactions with it.
    
    * Added debug mode to obstacle avoidance loop
    
    * Changed read_message from a loop to a single fire buffer scrape
    
    * Fix minor bugs
    
    * Fix big bugs
    
    I changed the delimiter from '\n' to '|' since '\n's were getting lost.
    Also made sure that we do not lose messages if we read while a message
    is being received and thus not get the entire message in one go. The
    partial message is stored and used during the next call to
    read_message().

--- src/code/communication/comms.py
+++ src/code/communication/comms.py
@@ -1,7 +1,12 @@
 #! /usr/bin/python3
 import serial
 import time
+import re
 
+__DEBUG__ = False
+
+ultra_pattern = re.compile('^<U[0-9]{1,10} [0-9]{1,10}>$')
+infra_pattern = re.compile('^<I[0-9]{1,10}( [0-9]{1,10}){4}>$')
 
 class Serial_Comm:
     def __init__(self, oport="/dev/ttyACM0", obaudrate=9600, otimeout=None):
@@ -9,6 +14,8 @@ class Serial_Comm:
 
         # Set the signal codes, identify the beginning and end of a signal
         self.no_sig = 4294967295
+
+        self.partial_msg = ''
 
     def read_message(self):
         '''
@@ -18,31 +25,40 @@ class Serial_Comm:
 
         # Message is essentially a list with some checking functions
         msgs = (None, None)
-        while self.ser.inWaiting() > 0:
-            msg = Message()
-            while not msg.is_closed():
+        inBuffer = self.ser.inWaiting()
+        if __DEBUG__: print(self.ser.inWaiting())
+        msg = ''
+        try:
+            # Read the entire buffer string and convert to list of strings
+            buf = self.partial_msg + self.ser.read(inBuffer).decode()
+            lines = buf.split('|')
+            if __DEBUG__: print(lines)  # VERY useful
+            if ultra_pattern.match(lines[-1]) or infra_pattern.match(lines[-1]):
+                self.partial_msg = ""
+            else:
+                self.partial_msg = lines[-1]
+
+            # Filter the lines according to regex pattern then take the bottom two
+            valid_msgs_rec = [line for line in lines
+                    if ultra_pattern.match(line) or infra_pattern.match(line)][-2:]
+
+            for msg in valid_msgs_rec:
                 try:
+                    msg = Message(msg)
+                except IOError:
+                    msg = ''
 
-                    # Receive the message as a string
-                    sm = self.ser.readline().strip()
-                    # Convert to integer opcode
-                    opcode = int(sm)
-                    self.ser.flush();
+                try:
+                    if msg.msg_type == 'ultrasonic':
+                        msgs = (msg, msgs[1])
+                    elif msg.msg_type == 'infrared':
+                        msgs = (msgs[0], msg)
+                except AttributeError:
+                    pass
 
-                    msg.append(opcode)
-
-                    if msg.is_well_formed():
-                        if msg.msg_type == 'ultrasonic':
-                            msgs = (msg, msgs[1])
-                        elif msg.msg_type == 'infrared':
-                            msgs = (msgs[0], msg)
-                        break
-
-                except ValueError:
-                    continue
-                except KeyboardInterrupt:
-                    print('Aborting Robot Operation')
-                    exit()
+        except KeyboardInterrupt:
+            print('Aborting Robot Operation')
+            exit()
         return msgs
 
     def send_to_serial(self, message):
@@ -62,88 +78,52 @@ class Serial_Comm:
 class Message(list):
     # Give the type of the message
     msg_type = None
-
-    open_message = 0xFFFFFFFE
-    close_message = 0xFFFFFFFD
-
-
-    ids = {'ultrasonic':0xFFFFFFFC,'infrared':0xFFFFFFFB}
     no_sig = 0xFFFFFFFF
     timestamp = 0
 
-    def is_well_formed(self):
-        'Checks if the message is fit to pass to planning'
-        if len(self) < 3: return False
+    def __init__(self, msg):
+        #STM Filtering data more
+        values = [int(d) for d in msg[2:-1].split(' ')]
+        self.timestamp = int(round(time.time() * 1000))
 
-        wf = True
-        if not self[0] == self.open_message: wf = False
-        if not self[-1] == self.close_message: wf = False
-        if self.get_left() == self.no_sig or self.get_right() == self.no_sig: wf = False
+        if ultra_pattern.match(msg):
+            self.msg_type = 'ultrasonic'
+            if __DEBUG__: print('Ultra message received: ', msg)
+            if self.no_sig in values:
+                raise IOError('Signal not received on one/both ultrasonic sensors')
+        elif infra_pattern.match(msg):
+            if __DEBUG__: print('Infra message received: ', msg)
+            self.msg_type = 'infrared'
+        else: 
+            raise IOError('Message was not well formed')
 
-        if not self[1] in self.ids: wf = False
+        for d in values:
+            self.append(d)
 
-        return wf
-
-
-    def append(self, item):
-        ''' 
-            Extension of list append assigns a type to the class
-        '''
-        if not self.is_open() and item != self.open_message:
-            return
-
-        if len(self) == 0:
-            self.timestamp = int(round(time.time() * 1000))
-
-        if len(self) > 2:
-            if self[1] == self.ids['ultrasonic']:
-                self.msg_type = 'ultrasonic'
-            if self[1] == self.ids['infrared']:
-                self.msg_type = 'infrared'
-
-        super(Message, self).append(item)
-
-    def is_open(self):
-        try:
-            if self[0] == self.open_message and self[-1] != self.close_message:
-                return True
-        except IndexError:
-            pass
-
-        return False
-
-    def is_closed(self):
-        try:
-            if self[-1] == self.close_message:
-                return True
-        except IndexError:
-            pass
-
-        return False
 
     def get_infrared_sensor_data(self, sensor_no):
         if self.msg_type != 'infrared':
-            raise ValueError
+            raise ValueError(self.msg_type)
 
-        return self[sensor_no+2]
+        return self[sensor_no]
 
     def get_left(self):
         if self.msg_type != 'ultrasonic':
-            raise ValueError
+            raise ValueError(self.msg_type)
 
-        return self[2]
+        return self[0]
 
     def get_right(self):
         if self.msg_type != 'ultrasonic':
             raise ValueError
 
-        return self[3]
+        return self[1]
 
 if __name__ == '__main__':
     ard = Serial_Comm()
 
     while True:
-        msgs = ard.read_from_serial()
+        msgs = ard.read_message()
         print('Ultrasonic: ', msg[0])
         print('Infrared: ', msg[1])
 
