140f8dd825c6dea17b01ea74ea4899a542cd069b
Alex Brisan <abrisan@exseed.ed.ac.uk>
2018-03-13 17:21:37 +0000

    remove app

--- app/app/src/main/java/helpers/Callback.java
+++
@@ -1,7 +0,0 @@
-package helpers;
-
-
-public interface Callback<C, T>
-{
-    public void accept(C class_object, T member);
-}
--- app/app/src/main/java/helpers/Constants.java
+++
@@ -1,7 +0,0 @@
-package helpers;
-
-
-public class Constants
-{
-    public static final String LOG_TAG = "Arnold";
-}
