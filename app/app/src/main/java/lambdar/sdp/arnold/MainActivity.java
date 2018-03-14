98a9bdcf8b4dd8d74a48764a3880a46d7d9c5c7c
Donal O'Shea <donaloshea@outlook.com>
2018-03-14 01:09:33 +0000

    Added app switch

--- app/app/src/main/java/lambdar/sdp/arnold/MainActivity.java
+++ app/app/src/main/java/lambdar/sdp/arnold/MainActivity.java
@@ -8,9 +8,12 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+//import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -36,9 +39,17 @@ public class MainActivity extends Activity
 
     private Button m_test_button;
     private MQTTHelper mHelper;
+    boolean alarm_flag=false;
     private NotificationChannel mChannel;
     private NotificationManager mManager;
     private Random prng = new Random(0);
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            =new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            return false;
+//        }
+//    };
     Map<String, TextView> fields = new HashMap<>();
     TextView previousView;
     private class MqttCallback implements Callback<MainActivity, MqttMessage>
@@ -56,18 +67,38 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Switch activateSwitch = (Switch)  findViewById(R.id.switch1);
-        TextView switchstatement = findViewById(R.id.manoraut);
+        Switch notificationSwitch = (Switch)  findViewById(R.id.switch2);
+        TextView followstatement = findViewById(R.id.manoraut);
+        TextView disturbStatement = findViewById(R.id.disturbia);
+//        BottomNavigationView navigation =findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         activateSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked==true){
-                    Log.i(LOG_TAG,"ON");
-                    switchstatement.setText("Automatic");
+                 //   Log.i(LOG_TAG,"ON");
+                    followstatement.setText("Follow");
                     mHelper.sendMessage("arnold/topics", "START");
                 }
                 else {
-                    Log.i(LOG_TAG,"OF");
-                    switchstatement.setText("Manual");
+                  //  Log.i(LOG_TAG,"OF");
+                    followstatement.setText("Do Not Follow");
                     mHelper.sendMessage("arnold/topics","STOP");
+                }
+            }
+        });
+
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked==true){
+                    //   Log.i(LOG_TAG,"ON");
+                    disturbStatement.setText("Out of Range Alarm");
+                    alarm_flag=true;
+
+                }
+                else {
+                    //  Log.i(LOG_TAG,"OF");
+                    disturbStatement.setText("No Alarm");
+                    alarm_flag=false;
                 }
             }
         });
@@ -83,7 +114,7 @@ public class MainActivity extends Activity
         this .mHelper = new MQTTHelper(this . getApplicationContext());
         this .mHelper. setCallback(new ArnoldMQTTCallback(this, new MqttCallback()));
 
-        this.fields.put(
+   /*     this.fields.put(
                 "KEY_UP_PRESSED",
                 (TextView) findViewById(R.id.up)
         );
@@ -101,7 +132,7 @@ public class MainActivity extends Activity
         this.fields.put(
                 "KEY_RIGHT_PRESSED",
                 (TextView) findViewById(R.id.right)
-        );
+        );*/
 
 
     }
@@ -118,7 +149,7 @@ public class MainActivity extends Activity
         String message_string = message.toString();
 
         Log.i(LOG_TAG, "Checking if to send notification");
-        if (message_string.equals("OR"))
+        if (message_string.equals("OR")&& alarm_flag) //only give notification if switch is on.
         {
             Log.i(LOG_TAG, "Sending notification");
             NotificationHelper.sendNotification(
